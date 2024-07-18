a0c8820f87839b409d61fa788ff83ba1e6c9a1f7
aotti <fattahjundi99@gmail.com>
2024-07-18 18:23:52 +0700

    remove JSON.stringify on register payload

--- src/app/components/main/components/RegisterPage.tsx
+++ src/app/components/main/components/RegisterPage.tsx
@@ -71,8 +71,8 @@ async function registerAccount(ev: FormEvent<HTMLFormElement>) {
     const formInputs = ([].slice.call(ev.currentTarget.elements) as any[]).filter(i => i.nodeName === 'INPUT')
     // get form input values
     const formData: IRegisterPayload = {
-        display_name: JSON.stringify(formInputs[0].value),
-        username: JSON.stringify(formInputs[1].value),
+        display_name: formInputs[0].value,
+        username: formInputs[1].value,
         password: sha256(formInputs[2].value),
         confirm_password: sha256(formInputs[3].value)
     }
