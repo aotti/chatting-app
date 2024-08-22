9b56e787f542c27ddca02c8834151da106cd79f7
aotti <fattahjundi99@gmail.com>
2024-08-22 19:22:11 +0700

    modify cache #6

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: "Cache-Control",
-                        value: "s-maxage=1, stale-while-revalidate=59"
+                        value: "public, s-maxage=1"
                     }
                 ]
             },
@@ -18,7 +18,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: "Cache-Control",
-                        value: "s-maxage=1, stale-while-revalidate=59"
+                        value: "public, s-maxage=1"
                     }
                 ]
             }
